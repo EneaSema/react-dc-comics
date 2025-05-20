d8ead0814edc46b21442fa54feb24315443f5d9c
EneaSema <sema.enea1993@gmail.com>
2025-05-20 21:11:44 +0200

    working-header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,25 @@
 export default function Header() {
-  return <header>Io sono header</header>;
+  return (
+    <header>
+      <div className="container">
+        <figure>
+          <img src="assets/img/dc-logo.png" alt="dc-logo" />
+        </figure>
+        <nav>
+          <ul>
+            <li>CHARACTERS</li>
+            <li id="text-selected">COMICS</li>
+            <li>MOVIES</li>
+            <li>TV</li>
+            <li>GAMES</li>
+            <li>COLLECTIBLES</li>
+            <li>VIDEOS</li>
+            <li>FANS</li>
+            <li>NEWS</li>
+            <li>SHOP</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
