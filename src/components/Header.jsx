38af1fc671ef890f604c58b43d542826b8e7ea90
EneaSema <sema.enea1993@gmail.com>
2025-05-20 21:54:19 +0200

    finish-header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,20 +3,40 @@ export default function Header() {
     <header>
       <div className="container">
         <figure>
-          <img src="assets/img/dc-logo.png" alt="dc-logo" />
+          <img src="../img/dc-logo.png" alt="dc-logo" />
         </figure>
         <nav>
           <ul>
-            <li>CHARACTERS</li>
-            <li id="text-selected">COMICS</li>
-            <li>MOVIES</li>
-            <li>TV</li>
-            <li>GAMES</li>
-            <li>COLLECTIBLES</li>
-            <li>VIDEOS</li>
-            <li>FANS</li>
-            <li>NEWS</li>
-            <li>SHOP</li>
+            <li>
+              <a href="#">CHARACTERS</a>
+            </li>
+            <li id="text-selected">
+              <a href="#">COMICS</a>
+            </li>
+            <li>
+              <a href="#">MOVIES</a>
+            </li>
+            <li>
+              <a href="#">TV</a>
+            </li>
+            <li>
+              <a href="#">GAMES</a>
+            </li>
+            <li>
+              <a href="#">COLLECTIBLES</a>
+            </li>
+            <li>
+              <a href="#">VIDEOS</a>
+            </li>
+            <li>
+              <a href="#">FANS</a>
+            </li>
+            <li>
+              <a href="#">NEWS</a>
+            </li>
+            <li>
+              <a href="#">SHOP</a>
+            </li>
           </ul>
         </nav>
       </div>
