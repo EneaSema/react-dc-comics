6ee25569ed22e0edcf5ce2000c6d26aea42f99e6
EneaSema <sema.enea1993@gmail.com>
2025-05-20 22:20:47 +0200

    finish-footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,45 @@
 export default function Footer() {
-  return <footer>Io sono footer</footer>;
+  return (
+    <footer>
+      <div className="container">
+        <button className="btn">
+          <h3 id="text-btn">SIGN-UP NOW!</h3>
+        </button>
+        <nav>
+          <ul>
+            <li>
+              <a id="text-follow-us" href="#">
+                FOLLOW US
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="../img/footer-facebook.png" alt="icona-facebook" />
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="../img/footer-twitter.png" alt="icona-twitter" />
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="../img/footer-youtube.png" alt="icona-youtube" />
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="../img/footer-pinterest.png" alt="icona-pintarest" />
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="../img/footer-periscope.png" alt="icona-periscope" />
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </footer>
+  );
 }
