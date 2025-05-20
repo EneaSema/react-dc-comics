3ba1fdce942b0be08bd6a0ad887f117f7d9d9a5b
EneaSema <sema.enea1993@gmail.com>
2025-05-20 22:50:45 +0200

    working-main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,29 @@
 export default function Main() {
-  return <main>Io sono main</main>;
+  return (
+    <main>
+      <div className="container-main">
+        <section className="section-insert">
+          <h1>--&gt; Content goes here &lt;--</h1>
+        </section>
+        <section className="section-merchandising">
+          <nav>
+            <ul>
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+              <li>4</li>
+              <li>5</li>
+            </ul>
+          </nav>
+        </section>
+        <section className="section-info">
+          <nav>
+            <ul>
+              <li></li>
+            </ul>
+          </nav>
+        </section>
+      </div>
+    </main>
+  );
 }
