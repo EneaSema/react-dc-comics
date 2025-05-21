eedb8f0b03efc581969057fab2d60b087cd7d8ba
EneaSema <sema.enea1993@gmail.com>
2025-05-21 14:17:23 +0200

    create-section-info-right

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,54 +9,118 @@ export default function Main() {
           <nav>
             <ul>
               <li>
-                <img
-                  className="img-icon-merchandise"
-                  src="../img/buy-comics-digital-comics.png"
-                  alt=""
-                />
-                <span className="text-icon-merchandise">DIGITAL COMICS</span>
+                <div className="card">
+                  <img
+                    className="img-icon-merchandise"
+                    src="../img/buy-comics-digital-comics.png"
+                    alt=""
+                  />
+                  <span className="text-icon-merchandise">DIGITAL COMICS</span>
+                </div>
               </li>
+
               <li>
-                <img
-                  className="img-icon-merchandise"
-                  src="../img/buy-comics-merchandise.png"
-                  alt=""
-                />
-                <span className="text-icon-merchandise">DIGITAL COMICS</span>
+                <div className="card">
+                  <img
+                    className="img-icon-merchandise"
+                    src="../img/buy-comics-merchandise.png"
+                    alt=""
+                  />
+                  <span className="text-icon-merchandise">DIGITAL COMICS</span>
+                </div>
               </li>
+
               <li>
-                <img
-                  className="img-icon-merchandise"
-                  src="../img/buy-comics-subscriptions.png"
-                  alt=""
-                />
-                <span className="text-icon-merchandise">DIGITAL COMICS</span>
+                <div className="card">
+                  <img
+                    className="img-icon-merchandise"
+                    src="../img/buy-comics-subscriptions.png"
+                    alt=""
+                  />
+                  <span className="text-icon-merchandise">DIGITAL COMICS</span>
+                </div>
               </li>
+
               <li>
-                <img
-                  className="img-icon-merchandise"
-                  src="../img/buy-comics-shop-locator.png"
-                  alt=""
-                />
-                <span className="text-icon-merchandise">DIGITAL COMICS</span>
+                <div className="card">
+                  <img
+                    className="img-icon-merchandise"
+                    src="../img/buy-comics-shop-locator.png"
+                    alt=""
+                  />
+                  <span className="text-icon-merchandise">DIGITAL COMICS</span>
+                </div>
               </li>
+
               <li>
-                <img
-                  className="img-icon-merchandise"
-                  src="../img/buy-dc-power-visa.svg"
-                  alt=""
-                />
-                <span className="text-icon-merchandise">DIGITAL COMICS</span>
+                <div className="card">
+                  <img
+                    className="img-icon-merchandise"
+                    src="../img/buy-dc-power-visa.svg"
+                    alt=""
+                  />
+                  <span className="text-icon-merchandise">DIGITAL COMICS</span>
+                </div>
               </li>
             </ul>
           </nav>
         </section>
+
         <section className="section-info">
-          <nav>
-            <ul>
-              <li></li>
-            </ul>
-          </nav>
+          <div id="dc-comics-shop">
+            <h4 className="info-text-important">DC COMICS</h4>
+            <nav>
+              <ul id="info-dc-comics">
+                <li>Characters</li>
+                <li>Comics</li>
+                <li>Movies</li>
+                <li>TV</li>
+                <li>Games</li>
+                <li>Videos</li>
+                <li>News</li>
+              </ul>
+            </nav>
+
+            <h4 className="info-text-important">SHOP</h4>
+            <nav>
+              <ul id="info-dc-comics">
+                <li>Shop DC</li>
+                <li>Shop DC Collectibles</li>
+              </ul>
+            </nav>
+          </div>
+
+          {/* <li>
+                <h4>DC</h4>
+                <nav>
+                  <ul id="info-dc-comics">
+                    <li>Terms Of Use</li>
+                    <li>Privacy policy</li>
+                    <li>Ad Choices</li>
+                    <li>Advertising</li>
+                    <li>Jobs</li>
+                    <li>Subscription</li>
+                    <li>Talent Workshops</li>
+                    <li>CPSC Certificates</li>
+                    <li>Ratings</li>
+                    <li>Shop Help</li>
+                    <li>Contact Us</li>
+                  </ul>
+                </nav>
+              </li> */}
+
+          {/* <li>
+                <h4>SHOP</h4>
+                <nav>
+                  <ul id="info-dc-comics">
+                    <li>DC</li>
+                    <li>MAD Magazine</li>
+                    <li>DC Kids</li>
+                    <li>DC Universe</li>
+                    <li>DC Power Visa</li>
+                  </ul>
+                </nav>
+              </li> */}
         </section>
       </div>
     </main>
